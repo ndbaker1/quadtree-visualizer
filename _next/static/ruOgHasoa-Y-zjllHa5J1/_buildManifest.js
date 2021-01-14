85910eeff6f12b6a183b6f9cddde0f6ff6185239
Nick Baker <ndbaker1@outlook.com>
2021-01-13 22:23:43 -0600

    Updates

--- _next/static/ruOgHasoa-Y-zjllHa5J1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fe869689b00fa228872ad.css","static\u002Fchunks\u002Fpages\u002Findex-7eb9158886c0f19cc7f1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
